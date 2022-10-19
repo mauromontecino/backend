dbb34240032ee98f130497510d58bc6b60392a97
mauromontecino <mauromontecino2@gmail.com>
2022-10-19 20:45:36 -0300

    entrega 3

---
+++ Entrega 3/serverExpress.js
@@ -0,0 +1,35 @@
+const Contenedor = require("./Contenedor");
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+const contenedor = new Contenedor("productos.json");
+
+app.get("/", (req, res) => {
+  res.send("Hi Express Server!");
+});
+
+app.get("/productos", async (req, res) => {
+  const allProducts = await contenedor.getAll();
+  res.json(allProducts);
+});
+
+app.get("/productoRandom", async (req, res) => {
+  const allProducts = await contenedor.getAll();
+  const maxId = allProducts.length;
+
+  const randomNumber = generateRandomNumber(1, maxId);
+  const randomProduct = await contenedor.getById(randomNumber);
+
+  res.json(randomProduct);
+});
+
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+};
+
+const server = app.listen(PORT, () => {
+  console.log(`>>>> Server started at http://localhost:${PORT}`);
+});
+
+server.on("error", (error) => console.log(error));
